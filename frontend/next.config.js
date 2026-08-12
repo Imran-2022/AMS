b25c71819201e312c432c796e47593832e48b831
MD IMRANUL HAQUE <mdimranulhaque27@gmail.com>
2026-08-12 07:27:08 +0600

    feat: containerize project with Docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   devIndicators: false,
   // Disable automatic enforcement of agent rules by Next.js (project-level opt-out)
   agentRules: false,
+  // Build standalone output for Docker
+  output: 'standalone',
   // devIndicators: {
   //   position: 'bottom-right',
   // },
